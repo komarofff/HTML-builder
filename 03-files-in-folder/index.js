d053103873ba9e29d5bd324bd6a5623c01a3c436
Anatoliy Komarov <admin@komaroff.biz>
2022-11-01 09:44:32 +0300

    feat: add comments to 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const directory = path.join(__dirname,'secret-folder')
+const directory = path.join(__dirname,'secret-folder') // определяем путь к папке
 
 try {
-    const files = fs.readdirSync(directory,{withFileTypes: true})
-    for (let file of files) {
-        if(file.isFile()) {
+    const files = fs.readdirSync(directory,{withFileTypes: true}) // читаем директорию и получаем данные
+    for (let file of files) { // перебираем объект
+        if(file.isFile()) { // если содержимое == файл двигаемся дальше
             //console.log(file.name)
-            fs.stat(directory + '/' + file.name, (err, stats) => {
+            fs.stat(directory + '/' + file.name, (err, stats) => {  // stat получает информацию о файле. Собираем и выводим в консоль
                 console.log(path.parse(file.name).name + ' - '  + path.extname(file.name).replace('.','') + ' - ' + stats.size/1000 + ' kb')
             })
         }
