de2a84788015298e6c5532038ea644536f46683a
Anatoliy Komarov <admin@komaroff.biz>
2022-11-01 08:22:30 +0300

    feat: finish task 01-read-file according of rules

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,18 @@
-const fs = require('fs')// подключаем модуль для работы с файлами и папками
-const path = require('path') // подключаем модуль для работы с путями (имена файлов, директорий, расширения)
-fs.readFile( // запускаем чтение
-    path.join(__dirname, 'text.txt'), // определяем имя читаемого файла (Конкатенация путей)
-    'utf-8',// ставим кодировку
-    (err, data) => { // используем асинхронный метод
-        if (err) throw err // при ошибке возвращаем error
-        console.log(data) // если нет ошибки - выводим данные
-    }
-)
+// const fs = require('fs')// подключаем модуль для работы с файлами и папками
+// const path = require('path') // подключаем модуль для работы с путями (имена файлов, директорий, расширения)
+// fs.readFile( // запускаем чтение
+//     path.join(__dirname, 'text.txt'), // определяем имя читаемого файла (Конкатенация путей)
+//     'utf-8',// ставим кодировку
+//     (err, data) => { // используем асинхронный метод
+//         if (err) throw err // при ошибке возвращаем error
+//         console.log(data) // если нет ошибки - выводим данные
+//     }
+// )
+// через ReadStream
+const fs = require('fs')
+const path = require('path')
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8')
+let data = ''
+stream.on('data', chunk => data += chunk)
+stream.on('end', () => console.log(data))
+stream.on('error', error => console.log('Error', error.message))
