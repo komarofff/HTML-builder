cafaa8dfb2c7f78cbe822f5c580ec9b07dc857c8
Anatoliy Komarov <admin@komaroff.biz>
2022-11-01 11:28:05 +0300

    feat: add comment in js file 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,55 +1,36 @@
 const promises = require('fs/promises');
 const fs = require('fs');
 const path = require('path')
-const startDirectory = path.join(__dirname, 'files')
-const finishDirectory = path.join(__dirname, 'files-copy')
+const startDirectory = path.join(__dirname, 'files') // source directory
+const finishDirectory = path.join(__dirname, 'files-copy') // destination directory
 
 
-fs.stat(finishDirectory, function (err) {
-    if (!err) {
+fs.stat(finishDirectory, function (err) { // проверяем есть ли destination directory
+    if (!err) { // если есть , то запускаем ассинхронную функцию по удалению файлов из дериктории и самой дериктории
         (async function () {
-            await fs.readdir(finishDirectory, (err, files) => {
+            await fs.readdir(finishDirectory, (err, files) => { // читаем папку
                 if (!err) {
                     for (let i = 0; i < files.length; i++) {
-                        fs.unlink(finishDirectory + "/" + files[i], (err) => {
+                        fs.unlink(finishDirectory + "/" + files[i], (err) => { // удаляем файлы
                             if (err) console.log(err)
                         })
                     }
-                    promises.rmdir(finishDirectory)
-                    copyFiles()
+                    promises.rmdir(finishDirectory) // удаляем папку
+                    copyFiles() // запускаем функцию создания папки заново и копирования файлов
                 }
             })
         })()
 
-    } else if (err.code === 'ENOENT') {
+    } else if (err.code === 'ENOENT') { // если папки нет -  запускаем функцию создания папки заново и копирования файлов
         copyFiles()
     }
 })
 
 async function copyFiles() {
-  await  promises.mkdir(finishDirectory, {recursive: true});
-   await fs.readdir(startDirectory, (err, files) => {
+  await  promises.mkdir(finishDirectory, {recursive: true}) // создаем папку назначения
+   await fs.readdir(startDirectory, (err, files) => { // читаем папку исходник
         for (let i = 0; i < files.length; i++) {
-            promises.copyFile(path.join(startDirectory, files[i]), path.join(finishDirectory, files[i]));
+            promises.copyFile(path.join(startDirectory, files[i]), path.join(finishDirectory, files[i])) // копируем файлы
         }
     })
 }
-
-//console.log('iiii',filesInFinish)
-//
-//
-// const fsPromises = require('fs/promises');
-// const path = require('path');
-// const folderPath = path.join(__dirname, 'files-copy');
-// const filesPath = path.join(__dirname, 'files');
-//
-// async function copyFiles() {
-//     fsPromises.mkdir(folderPath, {recursive: true});
-//     const files = await fsPromises.readdir(filesPath);
-//     for(let i = 0; i < files.length; i++){
-//         const srcFile = path.join(filesPath, files[i]);
-//         const destFile = path.join(folderPath, files[i]);
-//         fsPromises.copyFile(srcFile, destFile);
-//     }
-// }
-// copyFiles();
