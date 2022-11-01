debcf0d998f37169207cd24a63d8cc122fe129cd
Anatoliy Komarov <admin@komaroff.biz>
2022-11-01 09:49:22 +0300

    feat: add comments for task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const output = fs.createWriteStream(path.join(__dirname, 'writeText.txt'))
+const output = fs.createWriteStream(path.join(__dirname, 'writeText.txt')) // определяем записываемый файл , если его нет он создается
 const {stdin, stdout} = process
-stdout.write('Write text (to finish write exit or press CTR+C) : \n')
-stdin.on('data', data => {
-    let text = data.toString().trim()
-    if (text !== 'exit') {
-        output.write(data)
-    } else if (text === 'exit') {
-        process.exit()
+stdout.write('Hi! Write text (to finish write exit or press CTR+C) : \n') // выводим приветствие
+stdin.on('data', data => { // ждем ввод текста в консоли
+    let text = data.toString().trim() //  получаемый текст приводим в строку и удаляем пробелы в конце и начале строки
+    if (text !== 'exit') { //  если не exit
+        output.write(data) //  записываем в наш файл полученные данные
+    } else if (text === 'exit') { //  если написали exit
+        process.exit() //  выходим
     }
 })
-process.on('exit', () => stdout.write('God luck!\n'))
-process.on('SIGINT', () => {
+process.on('exit', () => stdout.write('God luck!\n')) //  когда выходим пишем прощание
+process.on('SIGINT', () => { // если нажали CTR+C  - выход
        process.exit()
 })
