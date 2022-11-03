e285c33c02e07908b9cf6277ae959556faf19a94
Anatoliy Komarov <admin@komaroff.biz>
2022-11-03 11:22:59 +0300

    feat: add comments to 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -17,64 +17,42 @@ const assetsFolderDestination = path.join(projectFolder, 'assets')
     //create index.html in destination folder
     await fs.promises.writeFile(projectIndexFile, '')
 
-    const templateData = await fs.promises.readFile(template, 'utf-8')
+    const templateData = await fs.promises.readFile(template, 'utf-8') // read template file
     let fullData = templateData
-    let result = templateData.match(/({{\w+}})/g).map(el => el.replace('{{', '').replace('}}', ''))
-    //console.log(result)
-    const templateFiles = await fs.promises.readdir(componentsFolder, 'utf-8')
+    let result = templateData.match(/({{\w+}})/g).map(el => el.replace('{{', '').replace('}}', '')) // get names of including templates in file
+    const templateFiles = await fs.promises.readdir(componentsFolder, 'utf-8') // read template directory
     for (let i = 0; i < result.length; i++) {
         let file = result[i] + '.html'
         templateFiles.forEach(el => {
-            if (path.extname(el) === '.html' && el === file) {
+            if (path.extname(el) === '.html' && el === file) { // ir we have a file and file is html and our template name equal of file name
                 (async function () {
-                    await fs.readFile(path.join(componentsFolder, el), 'utf-8', (err, data) => {
+                    await fs.readFile(path.join(componentsFolder, el), 'utf-8', (err, data) => { // read template file
                         if (err) throw err
-                        //fs.promises.appendFile(projectStyleFile, data)
-                        let replace = `{{${result[i]}}}`;
-                        let query = new RegExp(replace, "g");
-                        fullData = fullData.replace(query, data)
-                        fs.promises.writeFile(projectIndexFile, fullData)
+                        let replace = `{{${result[i]}}}` // make replace query
+                        let query = new RegExp(replace, "g")
+                        fullData = fullData.replace(query, data) // replace template
+                        fs.promises.writeFile(projectIndexFile, fullData) // rewrite destination file and save full result
                     })
                 })()
             }
         })
     }
-    // console.log(fullData)
-    // // create project directory
-    // await fs.promises.mkdir(projectFolder, {recursive: true})
-    // //create style.css in destination folder
-    // await fs.promises.writeFile(projectStyleFile, '')
-    //
-    // // read index file - template in var
-    // let templateData = await fs.promises.readFile(template, 'utf-8')
-    // // get header info
-    // const headerData = await fs.promises.readFile(path.join(componentsFolder, 'header.html'), 'utf-8')
-    // // get articles info
-    // const articlesData = await fs.promises.readFile(path.join(componentsFolder, 'articles.html'), 'utf-8')
-    // // get footer info
-    // const footerData = await fs.promises.readFile(path.join(componentsFolder, 'footer.html'), 'utf-8')
-    // // create fullData with changes
-    // let fullData = templateData.replace(/{{header}}/g, headerData).replace(/{{articles}}/g, articlesData).replace(/{{footer}}/g, footerData)
-    // // create destination file and push data
-    // await fs.promises.writeFile(projectIndexFile, fullData)
-    //
 
     // copy assets folder
-
     // create assets folder in project folder and copy all files to destination folder
     await fs.promises.mkdir(assetsFolderDestination, {recursive: true})
     const assetSourceData = await fs.promises.readdir(assetsFolderSource)
     assetSourceData.forEach(el => {
         fs.promises.mkdir(path.join(assetsFolderDestination, el), {recursive: true})
-        fs.readdir(path.join(assetsFolderSource, el), 'utf-8', (err, data) => {
-            data.forEach(elem => {
-                fs.promises.copyFile(path.join(path.join(assetsFolderSource, el), elem), path.join(path.join(assetsFolderDestination, el), elem)) // копируем файлы
+            fs.readdir(path.join(assetsFolderSource, el), 'utf-8', (err, data) => {
+                data.forEach(elem => {
+                    fs.promises.copyFile(path.join(path.join(assetsFolderSource, el), elem), path.join(path.join(assetsFolderDestination, el), elem)) // копируем файлы
+                })
             })
-        })
-
     })
 
     // styles
+    // read style folder and if file is css read file and put data to destination style.css file
     const styleFiles = await fs.promises.readdir(path.join(__dirname, 'styles'), 'utf-8')
     styleFiles.forEach(el => {
         if (path.extname(el) === '.css') {
