500adbb972a77ac58942fae5e2c52c0fae000d4e
Anatoliy Komarov <admin@komaroff.biz>
2022-11-03 11:10:19 +0300

    refactor: change script about read and write template in index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -14,19 +14,50 @@ const assetsFolderDestination = path.join(projectFolder, 'assets')
     await fs.promises.mkdir(projectFolder, {recursive: true})
     //create style.css in destination folder
     await fs.promises.writeFile(projectStyleFile, '')
+    //create index.html in destination folder
+    await fs.promises.writeFile(projectIndexFile, '')
 
-    // read index file - template in var
-    let templateData = await fs.promises.readFile(template, 'utf-8')
-    // get header info
-    const headerData = await fs.promises.readFile(path.join(componentsFolder, 'header.html'), 'utf-8')
-    // get articles info
-    const articlesData = await fs.promises.readFile(path.join(componentsFolder, 'articles.html'), 'utf-8')
-    // get footer info
-    const footerData = await fs.promises.readFile(path.join(componentsFolder, 'footer.html'), 'utf-8')
-    // create fullData with changes
-    let fullData = templateData.replace(/{{header}}/g, headerData).replace(/{{articles}}/g, articlesData).replace(/{{footer}}/g, footerData)
-    // create destination file and push data
-    await fs.promises.writeFile(projectIndexFile, fullData)
+    const templateData = await fs.promises.readFile(template, 'utf-8')
+    let fullData = templateData
+    let result = templateData.match(/({{\w+}})/g).map(el => el.replace('{{', '').replace('}}', ''))
+    //console.log(result)
+    const templateFiles = await fs.promises.readdir(componentsFolder, 'utf-8')
+    for (let i = 0; i < result.length; i++) {
+        let file = result[i] + '.html'
+        templateFiles.forEach(el => {
+            if (path.extname(el) === '.html' && el === file) {
+                (async function () {
+                    await fs.readFile(path.join(componentsFolder, el), 'utf-8', (err, data) => {
+                        if (err) throw err
+                        //fs.promises.appendFile(projectStyleFile, data)
+                        let replace = `{{${result[i]}}}`;
+                        let query = new RegExp(replace, "g");
+                        fullData = fullData.replace(query, data)
+                        fs.promises.writeFile(projectIndexFile, fullData)
+                    })
+                })()
+            }
+        })
+    }
+    // console.log(fullData)
+    // // create project directory
+    // await fs.promises.mkdir(projectFolder, {recursive: true})
+    // //create style.css in destination folder
+    // await fs.promises.writeFile(projectStyleFile, '')
+    //
+    // // read index file - template in var
+    // let templateData = await fs.promises.readFile(template, 'utf-8')
+    // // get header info
+    // const headerData = await fs.promises.readFile(path.join(componentsFolder, 'header.html'), 'utf-8')
+    // // get articles info
+    // const articlesData = await fs.promises.readFile(path.join(componentsFolder, 'articles.html'), 'utf-8')
+    // // get footer info
+    // const footerData = await fs.promises.readFile(path.join(componentsFolder, 'footer.html'), 'utf-8')
+    // // create fullData with changes
+    // let fullData = templateData.replace(/{{header}}/g, headerData).replace(/{{articles}}/g, articlesData).replace(/{{footer}}/g, footerData)
+    // // create destination file and push data
+    // await fs.promises.writeFile(projectIndexFile, fullData)
+    //
 
     // copy assets folder
 
@@ -56,7 +87,6 @@ const assetsFolderDestination = path.join(projectFolder, 'assets')
             })()
         }
     })
-
 
 
 })()
